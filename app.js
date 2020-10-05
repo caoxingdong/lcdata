978b1520609d5655e0d6aec1fed81fa285c20dfc
doudle <xingdong.cao@ucalgary.ca>
2020-10-05 06:31:00 +0000

    initial commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs")
 app.get("/", function(req, res){
 	res.render("home")
 })
-
-app.listen(3000, function() {
-	console.log("Services starts")
-})
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});
