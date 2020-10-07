b0a0443a8ce4dc58ad0ac669e459a41c5c12a327
doudle <xingdong.cao@ucalgary.ca>
2020-10-07 03:12:13 +0000

    contest pafe

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res){
+	res.render("home")
+})
+
+app.get("/contest", function(req, res){
 	request.get("https://leetcode.com/contest/api/ranking/weekly-contest-209/?pagination=1&region=global",function (err, httpResponse, body) {
 		if (!err && httpResponse.statusCode == 200) {	
 			data = JSON.parse(body).total_rank
-			console.log(data)
-			res.render("home", {data:data})
+			// console.log(data)
+			res.render("contest", {data:data})
 		}
 	})
-	// res.render("home")
 })
+
 let port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
